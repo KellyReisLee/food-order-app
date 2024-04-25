433dacaf4c9962f94af99a0a09904732b4f0e357
KellyReisLee <kristinaalvares28@gmail.com>
2024-04-25 17:12:39 +0100

    change

--- frontend/src/components/MealItem.jsx
+++ frontend/src/components/MealItem.jsx
@@ -11,14 +11,15 @@ export default function MealItem({ meal }) {
     cartCtx.addItem(meal);
   }
 
+
   return (
     <li className="meal-item">
       <article>
-        <img src={`https://food-order-app-api.vercel.app/${meal.image}`} alt={meal.name} />
+        <img src={meal.image} alt={meal.name} />
         <div>
           <h3>{meal.name}</h3>
           <p className="meal-item-price">
-            {currencyFormatter.format(meal.price)}
+            {currencyFormatter.format(`public/${meal.price}`)}
           </p>
           <p className="meal-item-description">{meal.description}</p>
         </div>
