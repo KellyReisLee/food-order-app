87f3fb4e8b1bef6be3eeb24cc2ecf3ffa5995380
KellyReisLee <kristinaalvares28@gmail.com>
2024-04-25 15:31:40 +0100

    changes-3

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ import cors from 'cors';
 
 const app = express();
 
-app.use(cors({ credentials: true, origin: "http://localhost:5173" }))
+app.use(cors({ credentials: true, origin: "https://food-order-app-front-rosy.vercel.app/" }))
 app.use(bodyParser.json());
 app.use('/public', express.static(__dirname + '/public'))
 
