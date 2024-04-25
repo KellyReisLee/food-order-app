92061e7316f330fcb9d4f7c9757b17c24374f6d6
KellyReisLee <kristinaalvares28@gmail.com>
2024-04-25 17:14:22 +0100

    change

--- frontend/src/components/MealItem.jsx
+++ frontend/src/components/MealItem.jsx
@@ -15,11 +15,11 @@ export default function MealItem({ meal }) {
   return (
     <li className="meal-item">
       <article>
-        <img src={meal.image} alt={meal.name} />
+        <img src={`public/${meal.image}`} alt={meal.name} />
         <div>
           <h3>{meal.name}</h3>
           <p className="meal-item-price">
-            {currencyFormatter.format(`public/${meal.price}`)}
+            {currencyFormatter.format(meal.price)}
           </p>
           <p className="meal-item-description">{meal.description}</p>
         </div>
