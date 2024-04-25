a31558d02f3c9cc2a7507a1bd74836c184fc7200
KellyReisLee <kristinaalvares28@gmail.com>
2024-04-25 16:11:57 +0100

    change-4.2

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ import cors from 'cors';
 
 const app = express();
 
-app.use(cors({ credentials: true, origin: "https://food-order-app-front-rosy.vercel.app" }));
+app.use(cors({ credentials: true, origin: "https://food-order-app-front-eww0sjzym-kellyreislees-projects.vercel.app" }));
 app.use(bodyParser.json());
 app.use('/public', express.static(__dirname + '/public'));
 
